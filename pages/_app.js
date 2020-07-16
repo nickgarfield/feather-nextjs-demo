242c5c1ae7f960f6699bc3c31fbbea9d25a58bff
nickgarfield <ntgarfield@gmail.com>
2020-07-16 16:23:59 -0500

    Add a stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { Feather } from "feather-client-react";
 import Home from "./home";
+import "../styles.css";
 
 function App({ Component, pageProps }) {
   return (
-    <Feather apiKey="pk_live_G92B6M6Gxo7QzpVcAM4BXHtcvDs3Sb">
+    <Feather apiKey="pk_live_Uyku8bQZZqQn8VngNYHK0IQS4AfdOJbe">
       <Home />
     </Feather>
   );
